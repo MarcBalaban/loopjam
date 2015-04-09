c7a8a465daef825a34289646baac56e513eb3c96
Marc Balaban <marc@marcbalaban.com>
2015-04-09 15:27:06 -0700

    close to finishing models and collections in backbone

--- client/src/app.js
+++ client/src/app.js
@@ -138,27 +138,27 @@
     //   this.createDownloadLink();
     //   this.recorder.clear();
     // },
-//     createDownloadLink: function() {
-//       this.recorder && this.recorder.exportWAV(function(blob) {
-//         var url = URL.createObjectURL(blob);
-//         var li = document.createElement('li');
-//         var au = document.createElement('audio');
-//         var hf = document.createElement('a');
+    // createDownloadLink: function() {
+    //   this.recorder && this.recorder.exportWAV(function(blob) {
+    //     var url = URL.createObjectURL(blob);
+    //     var li = document.createElement('li');
+    //     var au = document.createElement('audio');
+    //     var hf = document.createElement('a');
         
-//         data[counter].url = url;
-//         counter++;
+    //     data[counter].url = url;
+    //     counter++;
 
 
-//         au.controls = true;
-//         au.src = url;
-//         hf.href = url;
-//         hf.download = new Date().toISOString() + '.wav';
-//         hf.innerHTML = hf.download;
-//         li.appendChild(au);
-//         li.appendChild(hf);
-//         recordingslist.appendChild(li);
-//       });
-//     },
+    //     au.controls = true;
+    //     au.src = url;
+    //     hf.href = url;
+    //     hf.download = new Date().toISOString() + '.wav';
+    //     hf.innerHTML = hf.download;
+    //     li.appendChild(au);
+    //     li.appendChild(hf);
+    //     recordingslist.appendChild(li);
+    //   });
+    // },
 //     preBuffer: function(){
 //           // Initialize a new instance of the BufferLoader class,
 //         // passing in our context and data object. BufferLoader
@@ -313,71 +313,71 @@
 //     },
 
 
-//     play: function(button) {
-//       // Grab the value associated with the button,
-//       // will be used to identify the sound associated with the button.
-//       var soundIndex = button.value;
-//       console.log('soundIndex from play: ', soundIndex);
+    // play: function(button) {
+    //   // Grab the value associated with the button,
+    //   // will be used to identify the sound associated with the button.
+    //   var soundIndex = button.value;
+    //   console.log('soundIndex from play: ', soundIndex);
 
-//       // Grab the data object associated with the button.
-//       var soundData = data[soundIndex];
+    //   // Grab the data object associated with the button.
+    //   var soundData = data[soundIndex];
 
-//       console.log('playing a sound: ', soundData);
-//       // Grab the amount of time a bar takes to complete.
-//       var barTime = soundData.speed;
-//       var currentTime = this.context.currentTime;
+    //   console.log('playing a sound: ', soundData);
+    //   // Grab the amount of time a bar takes to complete.
+    //   var barTime = soundData.speed;
+    //   var currentTime = this.context.currentTime;
 
-//       // The remainder tells us how much of the bartime we have 
-//       // completed thus far.
-//       var remainder = currentTime % barTime;
-//       console.log('currentTime:', currentTime);
+    //   // The remainder tells us how much of the bartime we have 
+    //   // completed thus far.
+    //   var remainder = currentTime % barTime;
+    //   console.log('currentTime:', currentTime);
 
-//       // The delay calculates how much we'll have to delay
-//       // the playing of the sound so that we can perfectly
-//       // match the time of the next beat.
-//       var delay = barTime - remainder;
-//       console.log('delay: ', delay);
+    //   // The delay calculates how much we'll have to delay
+    //   // the playing of the sound so that we can perfectly
+    //   // match the time of the next beat.
+    //   var delay = barTime - remainder;
+    //   console.log('delay: ', delay);
 
-//       console.log('expected time of play: ', delay + currentTime);
+    //   console.log('expected time of play: ', delay + currentTime);
 
-//       // Reassign the source associated with the soundData object
-//       // to a new buffer source. This way, we can reference the same
-//       // source to stop playing the loop. However, once stopped,
-//       // the source is basically discarded and you must create
-//       // a new one. One play per source.
-//       var source = soundData.source = this.context.createBufferSource();
-//       console.log('source', source);
+    //   // Reassign the source associated with the soundData object
+    //   // to a new buffer source. This way, we can reference the same
+    //   // source to stop playing the loop. However, once stopped,
+    //   // the source is basically discarded and you must create
+    //   // a new one. One play per source.
+    //   var source = soundData.source = this.context.createBufferSource();
+    //   console.log('source', source);
 
-//       // Associate the new source instance with the loaded buffer.
-//       soundData.source.buffer = this.bufferLoader.bufferList[soundIndex];
-//       soundData.source.loop = true;
-//       // source.playbackRate.value = playbackControl.value;
+    //   // Associate the new source instance with the loaded buffer.
+    //   soundData.source.buffer = this.bufferLoader.bufferList[soundIndex];
+    //   soundData.source.loop = true;
+    //   // source.playbackRate.value = playbackControl.value;
 
-//       // Create a gainNode, through which we will pass the soundData.
-//       var gainNode = soundData.gainNode = this.context.createGain();
-//       // Connect the source to the gainNode.
-//       soundData.source.connect(gainNode);
+    //   // Create a gainNode, through which we will pass the soundData.
+    //   var gainNode = soundData.gainNode = this.context.createGain();
+    //   // Connect the source to the gainNode.
+    //   soundData.source.connect(gainNode);
 
-//       // Connect the gainNode to the destination.
-//       soundData.gainNode.connect(this.context.destination);
+    //   // Connect the gainNode to the destination.
+    //   soundData.gainNode.connect(this.context.destination);
 
-//       // Sets the playback rate to the value of bpm / rate of the bpm being recorded
-//       soundData.source.playbackRate.value = parseInt(this.bpm) / soundData.recordedAtBpm
+    //   // Sets the playback rate to the value of bpm / rate of the bpm being recorded
+    //   soundData.source.playbackRate.value = parseInt(this.bpm) / soundData.recordedAtBpm
 
-//       // Play the sound, delaying the sound by the delay necessary
-//       // to make the sound play at the start of a new measure.
-//       soundData.source.loopStart = soundData.source.buffer.duration - barTime;
-//       soundData.source.loopEnd = soundData.source.buffer.duration;
-//       var delayInMilliseconds = barTime * 1000 - parseInt(delay.toString().replace(/\./g,'').slice(0,4)) 
-//       soundData.source.start(currentTime + delay, soundData.source.buffer.duration - barTime, soundData.source.buffer.duration);
+    //   // Play the sound, delaying the sound by the delay necessary
+    //   // to make the sound play at the start of a new measure.
+    //   soundData.source.loopStart = soundData.source.buffer.duration - barTime;
+    //   soundData.source.loopEnd = soundData.source.buffer.duration;
+    //   var delayInMilliseconds = barTime * 1000 - parseInt(delay.toString().replace(/\./g,'').slice(0,4)) 
+    //   soundData.source.start(currentTime + delay, soundData.source.buffer.duration - barTime, soundData.source.buffer.duration);
           
 
-//       source.onended = function() {
-//         console.log('Your audio has finished playing');
-//       };
+    //   source.onended = function() {
+    //     console.log('Your audio has finished playing');
+    //   };
 
-//       console.log('source', source);
-//     },
+    //   console.log('source', source);
+    // },
 
 //     pause: function(button) {
 //       var soundIndex = button.value;
@@ -531,10 +531,10 @@ var TrackModel = Backbone.Model.extend({
   },
 
   initialize: function(params){
-    this.setAudioContext();
+    this.setAudioContext(params);
   },
 
-  setAudioContext: function(){ 
+  setAudioContext: function(params) { 
     var contextClass = (window.AudioContext || 
     window.webkitAudioContext || 
     window.mozAudioContext || 
@@ -569,10 +569,11 @@ var TrackModel = Backbone.Model.extend({
       
       this.set('loopNodes', params.loopNodes)
       
-      this.set('bufferLoader') = new BufferLoader(
+      this.set('bufferLoader', new BufferLoader(
         this.get('context'),
-        this.get('loopNodes')
-        );
+        this.get('loopNodes').giveUrls() // DEVELOP THIS METHOD OUTDEVELOP THIS METHOD OUTDEVELOP THIS METHOD OUT
+        )
+      );
       
       // Invoking bufferLoader's .load method does the actual
       // buffering and loading of the recordings, and stores
@@ -586,6 +587,163 @@ var TrackModel = Backbone.Model.extend({
       // Web Audio API is not available. Ask the user to use a supported browser.
       alert('Web Audio API is not available');
     }
+  },
+
+    startUserMedia: function(stream) {
+      console.log(this.get('context'))
+      var input = this.get('context').createMediaStreamSource(stream);
+      console.log('Media stream created.' );
+      console.log("input sample rate " +input.context.sampleRate);
+      
+      // input.connect(context.destination);
+      console.log('Input connected to audio context destination.');
+      
+      this.set('recorder', new Recorder(input));
+      console.log('Recorder initialised.');
+    },
+
+
+    recorderDelay: function(currentLoop) {
+
+      console.log('Starting Record:');
+      // Grab the amount of time a bar takes to complete.
+      var barTime = currentLoop.get('speed');
+      var tempo = this.get('tempo');
+      var currentTime = this.get('context').currentTime;
+      currentLoop.set('recordedAtBpm', this.get('tempo'));
+
+
+      console.log("barTime", barTime)
+
+      //Set up blank object for loop node      
+  
+      // var currentLoop = new LoopNodeModel({speed: barTime, reco})
+
+      // Sets up variables for loop node
+      
+      // The remainder tells us how much of the bartime we have 
+      // completed thus far.
+      var remainder = currentTime % barTime;
+
+      var delay = barTime - remainder;
+      
+      var delayInMilliseconds = parseInt(delay.toString().replace(/\./g,'').slice(0,4)) 
+      
+      console.log("Context Current-time", this.get('context').currentTime)
+      console.log("Record will start in:", delayInMilliseconds, "ms")
+      console.log("Record will stop in:", delayInMilliseconds + barTime * 1000, "ms")
+      
+      var barTimeInMS = barTime * 1000
+
+      setTimeout(this.startRecording.bind(this), delayInMilliseconds - 100)
+      setTimeout(this.stopRecording.bind(this, currentLoop), delayInMilliseconds + barTimeInMS + 50)
+      setTimeout(this.preBuffer.bind(this), delayInMilliseconds + barTimeInMS + 500)
+    },
+    
+    startRecording: function() {
+      console.log("time started recording:", this.get('context').currentTime)
+
+      this.get('recorder') && this.get('recorder').record();
+      // button.disabled = true;
+      // button.nextElementSibling.disabled = false;
+      console.log('Recording...');
+      console.time("recording1")
+    },
+
+    stopRecording: function(currentLoop) {
+          
+      console.log("time stopped recording:", this.context.currentTime)
+      this.get('recorder') && this.get('recorder').stop();
+      console.timeEnd("recording1")
+      // button.disabled = true;
+      // button.previousElementSibling.disabled = false;
+      console.log('Stopped recording.');
+      // create WAV download link using audio data blob
+      this.createDownloadLink(currentLoop);
+      this.get('recorder').clear();
+    },
+
+    createDownloadLink: function(currentLoop) {
+      this.recorder && this.recorder.exportWAV(function(blob) {
+        var url = URL.createObjectURL(blob);
+        
+        var li = document.createElement('li');
+        var au = document.createElement('audio');
+        var hf = document.createElement('a');
+        
+        currentLoop.set('url', url);
+        // counter++;
+
+
+        // au.controls = true;
+        // au.src = url;
+        // hf.href = url;
+        // hf.download = new Date().toISOString() + '.wav';
+        // hf.innerHTML = hf.download;
+        // li.appendChild(au);
+        // li.appendChild(hf);
+        // recordingslist.appendChild(li);
+      });
+    },
+
+    preBuffer: function(){
+          // Initialize a new instance of the BufferLoader class,
+        // passing in our context and data object. BufferLoader
+        // will buffer all of the recordings and hold onto
+        // references for the buffers.
+        this.set('bufferLoader', new BufferLoader(
+          this.context,
+          this.get('loopNodes').giveUrls() // NEED TO DEVELOP THIS OUT NEED TO DEVELOP THIS OUT
+          )
+        );
+
+        // Invoking bufferLoader's .load method does the actual
+        // buffering and loading of the recordings, and stores
+        // the buffers on the bufferloader instance.
+        this.bufferLoader.load();
+    },
+    // populateLoopNodes: function(){
+    //   // initialize loopnodes
+    //   var startAngle = 0; //starting angle should be 0
+    //   var radius = 150;
+
+    //   var loopnode1 = createLoopNode('.loopnode1', xPos(startAngle , radius), yPos(startAngle , radius));
+    //   var loopnode1Obj = {d3Obj: loopnode1, class: '.loopnode1', multiplier: 1};
+    //   var loopnode2 = createLoopNode('.loopnode2', xPos(startAngle , radius), yPos(startAngle , radius));
+    //   var loopnode2Obj = {d3Obj: loopnode2, class: '.loopnode2', multiplier: 1};
+    //   var loopnode3 = createLoopNode('.loopnode3', xPos(startAngle , radius), yPos(startAngle , radius));
+    //   var loopnode3Obj = {d3Obj: loopnode3, class: '.loopnode3', multiplier: 1};
+    //   var loopnode4 = createLoopNode('.loopnode4', xPos(startAngle , radius), yPos(startAngle , radius));
+    //   var loopnode4Obj = {d3Obj: loopnode4, class: '.loopnode4', multiplier: 1};
+    //   var loopnode5 = createLoopNode('.loopnode5', xPos(startAngle , radius), yPos(startAngle , radius));
+    //   var loopnode5Obj = {d3Obj: loopnode5, class: '.loopnode5', multiplier: 1};
+    //   var loopnode6 = createLoopNode('.loopnode6', xPos(startAngle , radius), yPos(startAngle , radius));
+    //   var loopnode6Obj = {d3Obj: loopnode6, class: '.loopnode6', multiplier: 1};
+    //   this.loopNodes.push(loopnode1Obj);
+    //   this.loopNodes.push(loopnode2Obj);
+    //   this.loopNodes.push(loopnode3Obj);
+    //   this.loopNodes.push(loopnode4Obj);
+    //   this.loopNodes.push(loopnode5Obj);
+    //   this.loopNodes.push(loopnode6Obj);
+
+
+    // },
+
+    changeTempo: function(bpm, t){
+          // bpm : the new tempo
+          // t : the audioCtx time when tempo changed
+          t = t || this.get('context').currentTime;
+
+          this.set('tempoAdjustment', this.get('tempoAdjustment') + t * (3/2) * (bpm - this.get('bpm')));
+          this.set('bpm', bpm);
+
+          _.each(this.get('loopNodes'), function(loopNode){
+            var currentSource = loopNode.get('source');
+            if(currentSource){
+              currentSource.playbackRate.value = parseInt(bpm) / loopNode.get('recordedAtBpm');
+            }    
+          })
+    },
 
   // this.tempoAdjustment = 0; //adjustment parameter when user changes tempo. initially set at 0.
   // this.bpm  = bpm || 120;
@@ -597,33 +755,41 @@ var TrackModel = Backbone.Model.extend({
   // this.populateLoopNodes();
   // this.setCueAnimation();
   // this.addListeners();
+  // },
+});
+
+var LoopNodeModel = Backbone.Model.extend({
+  defaults: {
+    url: '',
+    //refactor speed to barTime
+    speed: 2, 
+    source: null,
+    gainNode: null,
+    // port: loopNodeForTrack.nextPort(),
+    startPlayingTime: null,
+    endPlayingTime: null
   },
 
-  startUserMedia: function(stream) {
-    console.log(this.get('context'))
-    var input = this.get('context').createMediaStreamSource(stream);
-    console.log('Media stream created.' );
-    console.log("input sample rate " +input.context.sampleRate);
-    
-    // input.connect(context.destination);
-    console.log('Input connected to audio context destination.');
-    
-    this.set('recorder', new Recorder(input));
-    console.log('Recorder initialised.');
-  }
 
 
 });
 
 var LoopNodeCollection = Backbone.Collection.extend({
-  model: LoopNodeModel
+  model: LoopNodeModel,
+
+  // Assigns placeholder for where the track should be placed on the screen.
+  nextPort: function(){
+    if (!this.length) return 1;
+    return this.last().get('order') + 1
+  }
 });
 
-var LoopNodeModel = Backbone.Model.extend({});
-var loopNodeCollection = new LoopNodeCollection({model: loopNodeModel}) 
 
-var track = new TrackModel()
 
+// var loopNodesForTrack = new LoopNodeCollection([1,2,3,4,5,6]);
+
+var track = new TrackModel({});
+// loopNodesForTrack: params
 
 //   var recorder;
 //   var counter = 1;
